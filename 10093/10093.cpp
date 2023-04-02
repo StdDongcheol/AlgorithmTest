8e4d2e1af1310183215caac9fbee9a4bb0722455
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-03 00:28:36 +0900

    feat: 10093 solved

---
+++ 10093/10093.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+    long long A, B;
+
+    std::cin >> A >> B;
+
+    if (A > B)
+    {
+        unsigned long long Temp = A;
+        A = B;
+        B = Temp;
+    }
+
+    int Size = B - A - 1;
+
+    if (Size <= 1)
+        std::cout << "0";
+
+    else
+    {
+        std::cout << Size << "\n";
+
+        for (int i = 1; i <= Size; ++i)
+        {
+            std::cout << (unsigned long long)A + i << " ";
+        }
+    }
+
+    return 0;
+}
