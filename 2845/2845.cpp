0c41552629ab27442f5c10ff3989020fbcef868f
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-10 23:10:29 +0900

    feat: 2845 solved

---
+++ 2845/2845.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int L, P;
+    int Article[5] = { 0 };
+
+    std::cin >> L >> P;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cin >> Article[i];
+    }
+
+    int Players = L * P;
+
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << Article[i] - Players << " ";
+    }
+
+    return 0;
+}
