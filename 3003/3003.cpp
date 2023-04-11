f3acfee060db4db418f59a7a5670d28e1d7c7368
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-11 23:12:07 +0900

    feat: 3003 solved

---
+++ 3003/3003.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    int Arr[6] = { 0 };
+    int Chess[6] = { 1, 1, 2, 2, 2, 8 };
+
+    for (int i = 0; i < 6; ++i)
+    {
+        std::cin >> Arr[i];
+        Arr[i] = Chess[i] - Arr[i];
+    }
+
+    for (int i = 0; i < 6; ++i)
+    {
+        std::cout << Arr[i];
+    }
+
+
+    return 0;
+}
