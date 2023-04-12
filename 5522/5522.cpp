b49c5beade21ef2adfaf9d2e4fb0e7cd66677db1
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-12 23:50:48 +0900

    feat: 5522 solved

---
+++ 5522/5522.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int Number;
+    int Score = 0;
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cin >> Number;
+        Score += Number;
+    }
+
+    std::cout << Score;
+
+    return 0;
+}
