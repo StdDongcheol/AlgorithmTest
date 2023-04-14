ed7d6caa215fdcfc94f192141ca64dc966a1ce2a
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-14 23:38:49 +0900

    feat: 1225 solved

---
+++ 1225/1225.cpp
@@ -0,0 +1,27 @@
+﻿// 1225.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string A;
+    std::string B;
+    long long Sum = 0;
+
+    std::cin >> A >> B;
+    
+    for (int i = 0; i < A.length(); ++i)
+    {
+        for (int j = 0; j < B.length(); ++j)
+        {   
+            int ANumb = (int)A[i] - 48;
+            int BNumb = (int)B[j] - 48;
+
+            Sum += (ANumb * BNumb);
+        }
+    }
+
+    std::cout << Sum;
+}
