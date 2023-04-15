c9841239dc910bea8fe7dffaf7f94d4fcb1fc5d3
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-16 02:18:07 +0900

    feat: 2506 solved

---
+++ 2506/2506.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int N;
+    bool Answer[100] = { 0 };
+
+    std::cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> Answer[i];
+    }
+
+    bool Check = false;
+    int Sum = 0;
+    int Point = 1;
+    for (int i = 0; i < N; ++i)
+    {
+        if (Answer[i])
+        {
+            if (!Check)
+                Check = true;
+
+            Sum += Point;
+            ++Point;
+        }
+
+        else
+        {
+            Point = 1;
+            Check = false;
+        }
+    }
+
+    std::cout << Sum;
+
+    return 0;
+}
