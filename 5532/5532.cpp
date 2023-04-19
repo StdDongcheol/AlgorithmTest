6dab73f565472cef01cb9b5712083ef2dfc0c746
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-20 00:00:30 +0900

    feat: 5532 solved

---
+++ 5532/5532.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+    int L, A, B, C, D;
+    int day = 0;
+    std::cin >> L;
+    std::cin >> A;
+    std::cin >> B;
+    std::cin >> C;
+    std::cin >> D;
+
+    int KorDay;
+    int MathDay;
+
+    if (A % C != 0)
+        KorDay = (A / C) + 1;
+
+    else
+        KorDay = A / C;
+
+
+    if (B % D != 0)
+        MathDay = (B / D) + 1;
+
+    else
+        MathDay = B / D;
+
+    day = KorDay > MathDay ? KorDay : MathDay;
+
+    std::cout << L - day;
+
+    return 0;
+}
