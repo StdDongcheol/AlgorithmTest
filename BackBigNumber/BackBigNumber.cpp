c667e59b9de353fb0073f3bc855ab0684890c470
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-20 23:04:42 +0900

    feat: programmers solved

---
+++ BackBigNumber/BackBigNumber.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+
+int main()
+{
+	int N;
+	std::vector<int> vecNum;
+	std::stack<int> stkNGE;
+	std::vector<int> vecNGE;
+
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	std::cin >> N;
+
+	vecNum.resize(N + 1);
+	vecNGE.resize(N + 1);
+
+	for (int i = 1; i <= N; ++i)
+	{
+		std::cin >> vecNum[i];
+	}
+
+	vecNGE[N] = -1;
+	stkNGE.push(vecNum[N]);
+
+	for (int i = N - 1; i > 0; --i)
+	{
+		// 3 5 2 7
+		while (!stkNGE.empty() && stkNGE.top() <= vecNum[i])
+			stkNGE.pop();
+
+		if (stkNGE.empty())
+			vecNGE[i] = -1;
+		else
+			vecNGE[i] = stkNGE.top();
+
+		stkNGE.push(vecNum[i]);
+	}
+
+	for (int i = 1; i <= N; ++i)
+	{
+		std::cout << vecNGE[i] << " ";
+	}
+
+	return 0;
+}
---
+++ TargetNumber/TargetNumber.cpp
@@ -0,0 +1,46 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int TargetNumb;
+int answers = 0;
+
+void DFS(vector<int>& vecNum, int Sum, int Count)
+{
+    if (Count == vecNum.size())
+    {
+        if (TargetNumb == Sum)
+            ++answers;
+
+        return;
+    }
+
+    int AddSum = Sum + vecNum[Count];
+    int SubSum = Sum - vecNum[Count];
+
+    DFS(vecNum, AddSum, Count + 1);
+    DFS(vecNum, SubSum, Count + 1);
+}
+
+int solution(vector<int> numbers, int target)
+{
+    TargetNumb = target;
+
+    DFS(numbers, 0, 0);
+
+    return answers;
+}
+
+int main()
+{
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(1);
+    vec.push_back(1);
+    vec.push_back(1);
+    vec.push_back(1);
+    solution(vec, 3);
+
+    return 0;
+}
