ab402c4e171a52d38d13dd1561dd65555ca1bc37
local-JODONGCHEOL <highcore444@gmail.com>
2023-04-27 00:07:09 +0900

    feat: 4999 solved

---
+++ 4999/4999.cpp
@@ -0,0 +1,21 @@
+﻿// 4999.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string strDoc;
+    std::string strJae;
+
+    std::cin >> strDoc;
+    std::cin >> strJae;
+
+    if (strDoc.size() <= strJae.size())
+        std::cout << "go";
+    else
+        std::cout << "no";
+
+    return 0;
+}
