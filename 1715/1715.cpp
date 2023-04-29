04e4fc4ea4fddf0cc5b1e5c7e7bfbf758e717177
MSI_Jodongcheol <hicv555@naver.com>
2023-04-29 17:15:34 +0900

    feat: 1715 solved

---
+++ 1715/1715.cpp
@@ -0,0 +1,42 @@
+﻿// 1715.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+int main()
+{
+    long long Sum = 0;
+    int N;
+    std::vector<int> vecNum;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> pque;
+    std::cin >> N;
+
+    vecNum.resize(N, 0);
+
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> vecNum[i];
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
+        pque.push(vecNum[i]);
+    }
+
+    while (pque.size() != 1)
+    {
+        int Data = pque.top();
+        pque.pop();
+
+        int Data2 = pque.top();
+        pque.pop();
+
+        Sum += (long long)(Data + Data2);
+        pque.push(Data + Data2);
+    }
+
+    std::cout << Sum;
+}
