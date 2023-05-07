fc43e6879735bf4ae6eb419013207a4fd470e634
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-07 19:12:07 +0900

    feat: Emergency Boat solved

---
+++ EmergencyBoat/EmergencyBoat.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit)
+{
+    int answer = 0;
+    int start = 0;
+    int end = people.size() - 1;
+
+    std::sort(people.begin(), people.end());
+
+    // 단순한 two-pointer 탐색.
+    while (start <= end)
+    {
+        if (people[start] + people[end] <= limit)
+        {
+            ++start;
+            --end;
+        }
+        else
+            --end;
+
+        ++answer;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution({70, 80, 50, 50}, 100);
+}
