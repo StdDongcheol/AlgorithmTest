41a14050bf8c7220a3f9a265634cb66ac8e86ec1
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-12 22:32:05 +0900

    feat: 2309 solved

---
+++ 2309/2309.cpp
@@ -0,0 +1,49 @@
+﻿// 2309.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> vec;
+    std::vector<int> vecAnswer;
+    vec.resize(9);
+    for (int i = 0; i < 9; ++i)
+    {
+        std::cin >> vec[i];
+    }
+
+    int sum = 0;
+    do
+    {
+        sum = 0;
+
+        for (int i = 0; i < 7; ++i)
+        {
+            sum += vec[i];
+        }
+
+        if (sum == 100)
+            break;
+
+    } while (std::next_permutation(vec.begin(), vec.end()));
+
+    if (sum == 0)
+        std::cout << 0 << "\n";
+
+    else
+    {
+        for (int i = 0; i < 7; ++i)
+            vecAnswer.push_back(vec[i]);
+
+        std::sort(vecAnswer.begin(), vecAnswer.end());
+
+        for (int i = 0; i < 7; ++i)
+            std::cout << vecAnswer[i] << "\n";
+    }
+
+
+    return 0;
+}
