fa33493b6544e0a601331f42821ff6e8a295fc62
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-12 22:31:50 +0900

    feat: 2559 solved

---
+++ 2559/2559.cpp
@@ -0,0 +1,50 @@
+﻿// 2559.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int N, K;
+
+    std::vector<int> vecTemp;
+    std::vector<int> vec;
+    std::cin >> N >> K;
+
+    vecTemp.resize(N);
+    
+    for (int i = 0; i < N; ++i)
+        std::cin >> vecTemp[i];
+
+    int Start = 0;
+    int End = K;
+    int Sum = 0;
+
+    for (int i = Start; i < End; ++i)
+    {
+        Sum += vecTemp[i];
+    }
+    vec.push_back(Sum);
+
+
+    while (End < N)
+    {
+        Sum -= vecTemp[Start];
+        ++Start;
+
+        if (End < N)
+            Sum += vecTemp[End];
+        else
+            break;
+
+        ++End;
+
+        vec.push_back(Sum);
+    }
+    
+    std::cout << *std::max_element(vec.begin(), vec.end());
+
+    return 0;
+}
