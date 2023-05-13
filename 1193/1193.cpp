6574e274c1e615d540735b64d0454ee8cc9a0d81
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-13 23:35:26 +0900

    feat: 1193 solved

---
+++ 1193/1193.cpp
@@ -0,0 +1,32 @@
+﻿// 1193.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    int X = 0;
+    std::cin >> X;
+
+    int Row = 1;
+    while (true)
+    {
+        if ((Row * (Row + 1)) / 2 >= X)
+            break;
+
+        ++Row;
+    }
+
+    int Count = ((Row - 1) * Row) / 2;  // 이전 Row까지의 총 숫자 갯수
+    int i = X - Count;  // 현위치 X - 이전 Row 까지의 숫자 갯수
+    int j = Row + 1 - i;
+
+    if (Row % 2 == 0)
+        std::cout << i << "/" << j;
+
+
+    else
+        std::cout << j << "/" << i;
+
+    return 0;
+}
