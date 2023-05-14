748f3e2671345d55f07817303c62e8d4610cfb62
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-14 23:15:15 +0900

    feat: 5988 solved

---
+++ 5988/5988.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    std::vector<std::string> vecStr;
+
+    int N;
+    std::cin >> N;
+    vecStr.resize(N);
+
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> vecStr[i];
+
+        int Number = (int)(vecStr[i][vecStr[i].length() - 1] - '0');
+
+        if (Number % 2 == 0)
+            std::cout << "even\n";
+        else
+            std::cout << "odd\n";
+    }
+
+    return 0;
+}
