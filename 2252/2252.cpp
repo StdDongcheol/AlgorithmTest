15611d981a617ce6161d1619d9702404ec30cfb5
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-15 22:47:21 +0900

    feat: 2252 solved

---
+++ 2252/2252.cpp
@@ -0,0 +1,53 @@
+﻿// 2252.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+int main()
+{
+	int N, M;
+	std::vector<std::vector<int>> vecNode;
+	std::vector<int> vecInsert;
+	std::queue<int> queue;
+
+	std::cin >> N >> M;
+	vecNode.resize(N + 1);
+	vecInsert.resize(N + 1);
+
+	for (int i = 0; i < M; ++i)
+	{
+		int A, B;
+
+		std::cin >> A >> B;
+		vecNode[A].push_back(B);
+		++vecInsert[B];
+	}
+
+	for (int i = 1; i < vecInsert.size(); ++i)
+	{
+		if (vecInsert[i] == 0)
+			queue.push(i);
+	}
+
+	while (!queue.empty())
+	{
+		int Index = queue.front();
+		queue.pop();
+		
+		for (int i = 0; i < vecNode[Index].size(); ++i)
+		{
+			int NearNode = vecNode[Index][i];
+			--vecInsert[NearNode];
+
+			if (vecInsert[NearNode] == 0)
+				queue.push(NearNode);
+		}
+
+		std::cout << Index << " ";
+	}
+
+
+	return 0;
+}
