5c575d96f57729d8b7d6c164c4f7a5eba30b2e4e
Jodongcheol_Mobile <highcore444@gmail.com>
2023-05-15 16:16:05 +0900

    feat: PaperPlease solved

---
+++ 1920/소스.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+int arrN[100000] = {};
+int arrM[100000] = {};
+std::vector<int> vecAnswer;
+int main()
+{
+	int N, M;
+
+	std::cin >> N;
+	for (int i = 0; i < N; ++i)
+		std::cin >> arrN[i];
+
+	std::cin >> M;
+	for (int i = 0; i < M; ++i)
+		std::cin >> arrM[i];
+
+	std::sort(arrN, arrN + N);
+
+	for (int i = 0; i < M; ++i)
+	{
+		int Start = 0;
+		int End = N - 1;
+		int CurNumb = arrM[i];
+		
+		bool Check = false;
+		while (Start < End)
+		{
+			int Mid = (Start + End) / 2;
+
+			if (arrN[Mid] == CurNumb)
+			{
+				Check = true;
+				vecAnswer.push_back(1);
+				break;
+			}
+
+			if (CurNumb > arrN[Mid])
+			{
+				Start = Mid + 1;
+			}
+			else
+			{
+				End = Mid - 1;
+			}
+		}
+		if (!Check)
+			vecAnswer.push_back(0);
+	}
+
+	return 0;
+}
---
+++ PaperPlease/PaperPlease.cpp
@@ -0,0 +1,48 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times)
+{
+    long long answer = 0;
+    std::sort(times.begin(), times.end());
+
+    long long Left = 1;
+    long long Right = (long long)(times[times.size() - 1]) * n;
+
+    // 기초 Left = 0; Right = 60
+    // Mid = 30
+    while (Left <= Right)
+    {
+        long long Mid = (Left + Right) / 2;
+        long long Count = 0;
+
+
+        for (int i = 0; i < times.size(); ++i)
+        {
+            Count += Mid / times[i];
+        }
+
+        // n명 다 채웠다면
+        if (Count >= n)
+        {
+            answer = Mid;
+            Right = Mid - 1;
+        }
+
+        // Count가 n명을 다 못채웠다면
+        // Mid값 증가시켜서 Left를 갱신
+        else
+            Left = Mid + 1;
+
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(100000, { 7, 10 });
+}
