c8d3f5ad02d6d809fbf9ce05df7b6362db6c97dd
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-21 18:28:34 +0900

    feat: 1463 solved

---
+++ 1463/1463.cpp
@@ -0,0 +1,29 @@
+﻿// 1463.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int dp[1000001];
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+
+    for (int i = 4; i <= N; ++i)
+    {
+        dp[i] = dp[i - 1] + 1;
+
+        if (i % 2 == 0)
+            dp[i] = std::min(dp[i], dp[i / 2] + 1);
+        
+        if (i % 3 == 0)
+            dp[i] = std::min(dp[i], dp[i / 3] + 1);
+    }
+
+    std::cout << dp[N];
+}
