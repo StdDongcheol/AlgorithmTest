0d9a9596bb1174347cdef2fa4299c0381e8e47df
local-JODONGCHEOL <highcore444@gmail.com>
2023-05-26 22:23:02 +0900

    feat: 1654, MoreHot solved

---
+++ 1654/1654.cpp
@@ -0,0 +1,50 @@
+﻿// 1654.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	int N, K;
+	long long CurLength = 0;
+	std::vector<int> vecLength;
+	std::vector<int> vecK;
+
+	std::cin >> N >> K;
+
+	vecLength.resize(N);
+
+	for (int i = 0; i < N; ++i)
+		std::cin >> vecLength[i];
+
+	long long Start = 1;
+	long long End = *std::max_element(vecLength.begin(), vecLength.end());
+	long long Middle = 0;
+
+	while (Start <= End)
+	{
+		Middle = (Start + End) / 2;
+		int Count = 0;
+
+		for (int i = 0; i < N; ++i)
+			Count += (vecLength[i] / Middle);
+
+		if (Count >= K)
+		{
+			if (CurLength < Middle)
+				CurLength = Middle;
+			Start = Middle + 1;
+		}
+
+		else if (Count < K)
+		{
+			End = Middle - 1;
+		}
+	}
+
+	std::cout << CurLength;
+
+	return 0;
+}
--- _TestCase/_TestCase.cpp
+++ _TestCase/_TestCase.cpp
@@ -1,75 +1,35 @@
-﻿#include <string>
+﻿#include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 
-using namespace std;
-
-vector<int> solution(vector<string> genres, vector<int> plays)
+bool compare(const std::pair<int, std::string>& l, const std::pair<int, std::string>& r)
 {
-    vector<int> answer;
-    std::vector<std::pair<int, string>> vecChart;
-    std::map<string, int> mapPlayChart;
-    std::map<string, std::vector<std::pair<int, int>>> mapSongs;
-
-    for (int i = 0; i < genres.size(); ++i)
-    {
-        string strGenre = genres[i];
-        int iPlayCount = plays[i];
-
-        auto iter = mapPlayChart.find(strGenre);
-
-        if (iter != mapPlayChart.end())
-            iter->second += iPlayCount;
-        else
-            mapPlayChart.insert(make_pair(strGenre, iPlayCount));
-
-        mapSongs[strGenre].push_back(make_pair(iPlayCount, i));
-    }
-
-    {
-        auto iter = mapPlayChart.begin();
-        auto iterEnd = mapPlayChart.end();
-
-        for (; iter != iterEnd; ++iter)
-            vecChart.push_back(make_pair(iter->second, iter->first));
-
-        std::sort(vecChart.rbegin(), vecChart.rend());
-    }
-
-int n = 5;
-int k = 3;
-
-void combi(int Start, std::vector<int> vec, std::vector<int>& vecTarget)
-{
-    if (vec.size() == k)
-    {
-        for (int i = 0; i < vec.size(); ++i)
-            std::cout << vec[i] << " ";
-        std::cout << "\n";
-        return;
-    }
-
-    for (int i = Start + 1; i < n; ++i)
-    {
-        vec.push_back(vecTarget[i]);
-        combi(i, vec, vecTarget);
-        vec.pop_back();
-    }
+    return l.first < r.first;
 }
 
 int main()
 {
-    std::string strSrc = "블끼얏호우~!";
-    
-    std::string& strRef = strSrc;
-    
-    std::string strSrc2 = "돈스탑더뮤직~~!";
+    int N;
+    std::vector<std::pair<int, std::string>> vecN;
 
-    char* c = new char[40];
-    
-    std::vector<int> vec;
-    std::vector<int> vecTarget = { 5, 6, 7, 8, 9 };
+    std::cin >> N;
+    for (int i = 0; i < N; ++i)
+    {
+        int Age;
+        std::string Name;
 
-    combi(-1, vec, vecTarget);
+        std::cin >> Age >> Name;
+
+        vecN.push_back(std::make_pair(Age, Name));
+    }
+
+    std::stable_sort(vecN.begin(), vecN.end(), compare);
+
+    for (auto& i : vecN)
+    {
+        std::cout << i.first << " " << i.second << "\n";
+    }
 
     return 0;
 }
