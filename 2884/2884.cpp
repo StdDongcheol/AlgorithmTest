2970aed3234f7405bda0b42b27a15fb189fc4e30
local-JODONGCHEOL <highcore444@gmail.com>
2023-06-06 22:29:07 +0900

    feat: 2884 solved

---
+++ 2884/2884.cpp
@@ -0,0 +1,28 @@
+﻿// 2884.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int HH, MM;
+
+	std::cin >> HH >> MM;
+	
+	int LeftM = MM - 45;
+
+	if (LeftM < 0)
+	{
+		HH -= 1;
+		LeftM += 60;
+	}
+	MM = LeftM;
+
+	if (HH < 0)
+	{
+		HH = 24 + HH;
+	}
+	std::cout << HH << " " << MM;
+
+	return 0;
+}
