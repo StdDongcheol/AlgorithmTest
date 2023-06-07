1b4d2feb83bb92773a75c165ac01cafcacbb2fba
local-JODONGCHEOL <highcore444@gmail.com>
2023-06-07 15:06:52 +0900

    feat: Cache, MaxValueMinValue, 2562, 1157, 1152 solved

---
+++ 1157/1157.cpp
@@ -0,0 +1,46 @@
+﻿// 1157.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int Big[26];
+
+int main()
+{
+	std::string str;
+	std::cin >> str;
+
+	for (int i = 0; i < str.length(); ++i)
+	{
+		int AlphaNumb = static_cast<int>(str[i]);
+
+		if(AlphaNumb - 97 >= 0)
+			++Big[AlphaNumb - 97];
+		else
+			++Big[AlphaNumb - 65];
+	}
+
+	int Count = 0;
+	int index = 0;
+	int MostNumb = 0;
+	MostNumb = *std::max_element(Big, Big + 26);
+
+	for (int i = 0; i < 26; ++i)
+	{
+		if (MostNumb == Big[i])
+		{
+			++Count;
+			index = i;
+		}
+	}
+
+	if (Count >= 2)
+		std::cout << "?";
+	else
+		std::cout << static_cast<char>(index + 65);
+
+	return 0;
+}
+
---
+++ 2562/2562.cpp
@@ -0,0 +1,26 @@
+﻿// 2562.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+
+int Numb[10] = {};
+
+int main()
+{
+	for (int i = 1; i <= 9; ++i)
+	{
+		int N;
+		std::cin >> N;
+		Numb[i] = N;
+	}
+	int Max = *std::max_element(Numb + 1, Numb + 10);
+	
+	std::cout << Max << '\n';
+
+	for (int i = 1; i <= 9; ++i)
+	{
+		if (Max == Numb[i])
+			std::cout << i;
+	}
+}
