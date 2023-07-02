2348f445072f3a95fe23c0823747048b18d63757
local-JODONGCHEOL <highcore444@gmail.com>
2023-07-02 22:03:02 +0900

    feat: 2110 solved

---
+++ 2110/2110.cpp
@@ -0,0 +1,52 @@
+﻿// 2110.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int arrN[200000];
+
+int main()
+{
+	int N, M;
+	int MaxLength = 0;
+	std::cin >> N >> M;
+
+	for (int i = 0; i < N; ++i)
+		std::cin >> arrN[i];
+
+	std::sort(arrN, arrN + N);
+
+	int Start = 1;
+	int End = arrN[N - 1] - arrN[0];
+	
+	while (Start <= End)
+	{
+		int Middle = (Start + End) / 2;
+		int StartIdx = arrN[0];
+		int Count = 1;
+
+		for (int i = 1; i < N; ++i)
+		{
+			if (arrN[i] - StartIdx >= Middle)
+			{
+				StartIdx = arrN[i];
+				++Count;
+			}
+		}
+
+		if (Count >= M)
+		{
+			MaxLength = std::max(MaxLength, Middle);
+			Start = Middle + 1;
+		}
+
+		else
+			End = Middle - 1;
+	}
+
+	std::cout << MaxLength;
+
+	return 0;
+}
