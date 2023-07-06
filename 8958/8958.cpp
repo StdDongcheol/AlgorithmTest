8bf1233a98ca4f46a2a2d843b869ce5fb0314ca8
local-JODONGCHEOL <highcore444@gmail.com>
2023-07-06 16:49:11 +0900

    feat: 8958 solved

---
+++ 8958/8958.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+
+        int score = 0;
+        int CurScore = 0;
+        std::string str;
+        std::cin >> str;
+
+        for (int j = 0; j < str.length(); ++j)
+        {
+            if (str[j] == 'O')
+            {
+                ++score;
+                CurScore += score;
+            }
+            else
+            {
+                score = 0;
+            }
+        }
+        std::cout << CurScore << '\n';
+    }
+
+    return 0;
+}
