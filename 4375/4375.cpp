88f42acc1562ed96b2ae00c6432458312566e00e
Jodongcheol <highcore444@gmail.com>
2023-07-17 17:25:28 +0900

    feat: 4375 solved

---
+++ 4375/4375.cpp
@@ -0,0 +1,26 @@
+﻿// 4375.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	int N;
+
+	while (std::cin >> N)
+	{
+		int Numb = 0;
+		int Count = 0;
+
+		while (true)
+		{
+			Count++;
+			Numb = (Numb * 10 + 1) % N;
+			if (Numb == 0)
+				break;
+		}
+
+		std::cout << Count << '\n';
+	}
+	return 0;
+}
