2bc12cd943e4e4b3ec3c8bb23b2c2f43247e609c
Jodongcheol <highcore444@gmail.com>
2023-08-01 21:39:03 +0900

    feat: 1697 solved

---
+++ 1697/1697.cpp
@@ -0,0 +1,50 @@
+﻿// 1697.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <queue>
+
+int arr[200001];
+bool check[200001];
+
+int main()
+{
+	std::queue<int> q;
+	int N, K, X;
+	int count = 0;
+	std::cin >> N >> K;
+	X = N;
+
+	q.push(X);
+	arr[X] = 0;
+	check[X] = true;
+
+	while (!q.empty()) 
+	{
+		int CurX = q.front();
+		q.pop();
+
+		if (CurX - 1 >= 0 && !check[CurX - 1])
+		{
+			q.push(CurX - 1);
+			check[CurX - 1] = true;
+			arr[CurX - 1] = arr[CurX] + 1;
+		}
+		if (CurX + 1 < 200001 && !check[CurX + 1])
+		{
+			q.push(CurX + 1);
+			check[CurX + 1] = true;
+			arr[CurX + 1] = arr[CurX] + 1;
+		}
+		if (CurX * 2 < 200001 && !check[CurX * 2])
+		{
+			q.push(CurX * 2);
+			check[CurX * 2] = true;
+			arr[CurX * 2] = arr[CurX] + 1;
+		}
+	}
+
+	std::cout << arr[K];
+
+	return 0;
+}
