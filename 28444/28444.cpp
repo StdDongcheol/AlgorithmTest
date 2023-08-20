3e787af0c564db6b9ad1ff2598afbb32f99180a9
Jodongcheol <highcore444@gmail.com>
2023-08-20 23:57:29 +0900

    feat: 28444 solved

---
+++ 28444/28444.cpp
@@ -0,0 +1,15 @@
+﻿// 28444.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    int H, I, A, R, C;
+
+    std::cin >> H >> I >> A >> R >> C;
+
+    std::cout << (H * I) - (A * R * C);
+
+    return 0;
+}
