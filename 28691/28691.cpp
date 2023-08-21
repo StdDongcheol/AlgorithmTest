62249391c59b98cc9bf6154672df21cb1e91deae
Jodongcheol <highcore444@gmail.com>
2023-08-22 00:10:37 +0900

    feat: 28691 solved

---
+++ 28691/28691.cpp
@@ -0,0 +1,31 @@
+﻿// 28691.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+    char c;
+    std::cin >> c;
+
+    switch (c)
+    {
+    case 'M':
+        std::cout << "MatKor";
+        break;
+    case 'W':
+        std::cout << "WiCys";
+        break;
+    case 'C':
+        std::cout << "CyKor";
+        break;
+    case 'A':
+        std::cout << "AlKor";
+        break;
+    case '$':
+        std::cout << "$clear";
+        break;
+    }
+
+    return 0;
+}
