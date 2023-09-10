9928a7c14c490332dd1b50797fd6d2d61fc4c448
Jodongcheol <highcore444@gmail.com>
2023-09-10 22:57:04 +0900

    feat: 10867 solved

---
+++ 10867/10867.cpp
@@ -0,0 +1,26 @@
+﻿// 10867.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int N;
+    std::cin >> N;
+    std::vector<int> vec(N);
+    for (int i = 0; i < N; ++i)
+        std::cin >> vec[i];
+
+    std::sort(vec.begin(), vec.end());
+    vec.erase(std::unique(vec.begin(), vec.end()), vec.end());
+
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        std::cout << vec[i] << ' ';
+    }
+
+
+    return 0;
+}
