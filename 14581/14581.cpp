a3b98ef0e058872afcf2203a471acc53393a9b15
Jodongcheol <highcore444@gmail.com>
2023-09-29 20:34:35 +0900

    feat: 14581 solved

---
+++ 14581/14581.cpp
@@ -0,0 +1,17 @@
+﻿// 14581.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str;
+    std::cin >> str;
+
+    std::cout << ":fan::fan::fan:\n";
+    std::cout << ":fan::" << str << "::fan:\n";
+    std::cout << ":fan::fan::fan:\n";
+
+    return 0;
+}
