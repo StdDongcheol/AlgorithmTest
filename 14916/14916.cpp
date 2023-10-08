3e1442dcfa350adf5c213b282dfa2e6da73edf44
Jodongcheol <highcore444@gmail.com>
2023-10-08 17:10:25 +0900

    feat: 3135, 14916 solved

---
+++ 14916/14916.cpp
@@ -0,0 +1,33 @@
+﻿// 14916.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+
+int arr[100001];
+
+int main()
+{
+    int N;
+    std::cin >> N;
+    std::fill(arr, arr + N + 1, -1);
+    
+    arr[2] = 1;
+    arr[5] = 1;
+
+    for (int i = 4; i <= N; ++i)
+    {
+        if (arr[i - 2] != -1)
+        {
+            arr[i] = arr[i - 2] + 1;
+        }
+        if (i >= 5 && arr[i - 5] != -1)
+        {
+            arr[i] = std::min(arr[i - 5] + 1, arr[i]);
+        }
+    }
+
+    std::cout << arr[N];
+
+    return 0;
+}
---
+++ 3135/3135.cpp
@@ -0,0 +1,33 @@
+﻿// 3135.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int N;
+    int A, B;
+    
+    std::cin >> A >> B;
+    std::cin >> N;
+    std::vector<int> vec(N);
+    
+    bool Check = false;
+    for (int i = 0; i < N; ++i)
+    {
+        std::cin >> vec[i];
+        if (abs(vec[i] - B) < abs(A - B))
+        {
+            Check = true;
+            A = vec[i];
+        }
+    }
+
+    if (Check)
+        std::cout << abs(A - B) + 1;
+    else
+        std::cout << abs(A - B);
+
+    return 0;
+}
