b2cb13ae7ce8ca10d30d22e027c0baeb5284a5a8
Jodongcheol <highcore444@gmail.com>
2023-10-15 22:57:56 +0900

    feat: 14469 solved

---
+++ 14469/14469.cpp
@@ -0,0 +1,39 @@
+﻿// 14469.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	int N;
+	std::cin >> N;
+
+	std::vector<std::pair<int, int>> vec(N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		std::cin >> vec[i].first >> vec[i].second;
+	}
+	
+	std::sort(vec.begin(), vec.end());
+
+	int CurTime = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		int EnterTime = vec[i].first;
+		if (CurTime < EnterTime)
+		{
+			CurTime = vec[i].first + vec[i].second;
+		}
+		else
+		{
+			CurTime += vec[i].second;
+		}
+	}
+
+	std::cout << CurTime;
+
+	return 0;
+}
