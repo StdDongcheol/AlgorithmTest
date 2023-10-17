19d853695f006d98a8a143ebe8666d4bf6b1a1f6
Jodongcheol <highcore444@gmail.com>
2023-10-17 23:59:59 +0900

    feat: 2744 solved

---
+++ 2744/2744.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str;
+    std::cin >> str;
+
+    for (int i = 0; i < str.length(); ++i)
+    {
+        int Curchar = (int)str[i];
+        if (Curchar >= 97)
+        {
+            std::cout << (char)toupper(Curchar);
+        }
+        else
+        {
+            std::cout << (char)tolower(Curchar);
+        }
+    }
+
+    return 0;
+}
