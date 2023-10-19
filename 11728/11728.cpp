02144a4c871987fd6fdf47db4fc1b61b3754e3ea
Jodongcheol <highcore444@gmail.com>
2023-10-19 20:39:56 +0900

    feat: 11728, 2531 solved

---
+++ 11728/11728.cpp
@@ -0,0 +1,59 @@
+﻿// 11728.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int N, M;
+    std::cin >> N >> M;
+    std::vector<int> vecN(N);
+    std::vector<int> vecM(M);
+
+    for (int i = 0; i < N; ++i)
+        std::cin >> vecN[i];
+
+    for (int i = 0; i < M; ++i)
+        std::cin >> vecM[i];
+
+    int NIndex = 0;
+    int MIndex = 0;
+    std::vector<int> result;
+
+    while (NIndex != N && MIndex != M)
+    {
+        int LowVal = std::min(vecN[NIndex], vecM[MIndex]);
+        result.push_back(LowVal);
+
+        if (LowVal == vecN[NIndex])
+            ++NIndex;
+        else
+            ++MIndex;
+    }
+
+    if (NIndex == N)
+    {
+        while (MIndex != M)
+        {
+            result.push_back(vecM[MIndex]);
+            ++MIndex;
+        }
+    }
+    else
+    {
+        while (NIndex != N)
+        {
+            result.push_back(vecN[NIndex]);
+            ++NIndex;
+        }
+    }
+
+    for (int i : result)
+        std::cout << i << ' ';
+
+    return 0;
+}
---
+++ 2531/2531.cpp
@@ -0,0 +1,69 @@
+﻿// 2531.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <queue>
+#include <deque>
+
+int sushi[3001];
+
+int main()
+{
+    // d : 초밥가짓수, k: 연속접시수, c:쿠폰번호
+    int N, d, k, c;
+    std::queue<int> q;
+    std::deque<int> dq;
+
+    std::cin >> N >> d >> k >> c;
+    for (int i = 0; i < N; ++i)
+    {
+        int Numb;
+        std::cin >> Numb;
+        q.push(Numb);
+    }
+
+    ++sushi[c];
+    int MaxCount = 0;
+    int CurCount = 1;
+    for (int i = 0; i < k; ++i)
+    {
+        int CurSushi = q.front();
+        q.pop();
+        
+        if (sushi[CurSushi] == 0)
+        {
+            ++CurCount;
+        }
+        ++sushi[CurSushi];
+        dq.push_back(CurSushi);
+    }
+
+    MaxCount = CurCount;
+
+    for (int i = 1; i < N; ++i)
+    {
+        if (q.empty())
+            break;
+
+        int FrontSushi = dq.front();
+        dq.pop_front();
+        --sushi[FrontSushi];
+        if (sushi[FrontSushi] == 0)
+            --CurCount;
+        q.push(FrontSushi);
+
+        int CurSushi = q.front();
+        q.pop();
+        if (sushi[CurSushi] == 0)
+        {
+            ++CurCount;
+        }
+        ++sushi[CurSushi];
+        dq.push_back(CurSushi);
+        MaxCount = std::max(MaxCount, CurCount);
+    }
+
+    std::cout << MaxCount;
+
+    return 0;
+}
