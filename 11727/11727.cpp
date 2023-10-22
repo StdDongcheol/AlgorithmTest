221039f8feb16b2f0aa209c6737f89a185e78236
Jodongcheol <highcore444@gmail.com>
2023-10-22 20:29:32 +0900

    feat: 11727 solved

---
+++ 11727/11727.cpp
@@ -0,0 +1,24 @@
+﻿// 11727.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+long long arr[1001];
+
+int main()
+{
+	int N;
+	std::cin >> N;
+	
+	arr[1] = 1;
+	arr[2] = 3;
+
+	for (int i = 3; i <= N; ++i)
+	{
+		arr[i] = ((arr[i - 2] * 2) + arr[i - 1]) % 10007;
+	}
+
+	std::cout << arr[N];
+
+	return 0;
+}
