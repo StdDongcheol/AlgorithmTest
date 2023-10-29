b24151225efe7032e682d9c1cadeaefa408a169b
Jodongcheol <highcore444@gmail.com>
2023-10-29 20:59:44 +0900

    feat: 1197 solved

---
+++ 15681/15681.cpp
@@ -0,0 +1,16 @@
+﻿// 15681.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <list>
+
+struct Node
+{
+    int Numb;
+    std::list<Node> listNode;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
