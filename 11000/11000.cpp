efa2be738e1d64fe1b64fce9826b705919cffbc2
Jodongcheol <highcore444@gmail.com>
2023-12-04 15:56:42 +0900

    feat: 11000 resolved, 2812 solved

--- 11000/11000.cpp
+++ 11000/11000.cpp
@@ -1,44 +1,43 @@
-﻿// 11000.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <queue>
 
 int N;
 
 int main()
 {
-	int CurCount = 1;
 	std::cin >> N;
-	std::priority_queue<int, std::vector<int>, std::greater<int>> pq1;
-	std::vector<std::pair<int, int>> vec(N);
-	
+	std::vector<std::pair<int, int>> vec;
 	for (int i = 0; i < N; ++i)
 	{
 		int S, T;
 		std::cin >> S >> T;
-		vec[i] = { S, T };
+		vec.push_back({ S, T });
 	}
-	
+
 	std::sort(vec.begin(), vec.end());
 
-	pq1.push(vec[0].second);
-	
-	for (int i = 1; i < vec.size(); ++i)
-	{
-		int CurEnd = pq1.top();
+	std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
 
-		if (CurEnd <= vec[i].first)
+	pq.push(vec[0].second);
+
+	for (int i = 1; i < N; ++i)
+	{
+		int CurEnd = pq.top();
+		int NextStart = vec[i].first;
+
+		if (CurEnd <= NextStart)
 		{
-			pq1.pop();
+			pq.pop();
 		}
 
-		pq1.push(vec[i].second);
+		pq.push(vec[i].second);
 	}
-	
-	std::cout << pq1.size();
+
+	std::cout << pq.size();
 
 	return 0;
 }
---
+++ 2812/2812.cpp
@@ -0,0 +1,79 @@
+﻿// 2812.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+
+int main()
+{
+    int N, K;
+    std::string str;
+    std::cin >> N >> K;
+    std::cin >> str;
+    
+    std::stack<int> stkStr;
+    std::stack<int> stk;
+   
+    for(int i = str.length() - 1; i >= 0; --i)
+    {
+        stkStr.push((int)str[i] - '0');
+    }
+
+    while (K > 0 && !stkStr.empty())
+    {
+        int NextNumb = stkStr.top();
+        stkStr.pop();
+
+        if (stk.empty())
+        {
+            stk.push(NextNumb);
+            continue;
+        }
+
+        else 
+        {
+            while(!stk.empty() && NextNumb > stk.top())
+            {
+                if (K <= 0)
+                    break;
+
+                stk.pop();
+                --K;
+            }
+            
+            stk.push(NextNumb);
+        }
+    }
+
+    std::string NumbStr;
+    while (!stk.empty())
+    {
+        int Numb = stk.top();
+        stk.pop();
+        NumbStr += std::to_string(Numb);
+    }
+
+    std::reverse(NumbStr.begin(), NumbStr.end());
+
+    while (!stkStr.empty())
+    {
+        int Numb = stkStr.top();
+        stkStr.pop();
+        NumbStr += std::to_string(Numb);
+    }
+
+    if (K > 0)
+    {
+        while (K > 0)
+        {
+            NumbStr.pop_back();
+            --K;
+        }
+    }
+
+    std::cout << NumbStr;
+
+    return 0;
+}
